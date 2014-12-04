363018cc6a1fe8f46fc779ee67e8a1ab0039f4e8
apleaf <c16alexander.leaf@usafa.edu>
2014-12-04 10:50:59 -0700

    added .h file

---
+++ robot.h
@@ -0,0 +1,17 @@
+#ifndef ROBOT_H_
+#define ROBOT_H_
+
+
+
+
+void initTimerOutputSignals();
+void configureA0andA1Timers();
+void clearModes();
+void turnSmallRight();
+void turnBigRight();
+void turnSmallLeft();
+void turnBigLeft();
+void moveForward();
+void moveBackward();
+
+#endif /* ROBOT_H_ */
